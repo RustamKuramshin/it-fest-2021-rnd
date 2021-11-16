9fca8a23925c2b1545c7563ccb7c3377c77df85c
Margarita Sosnovskaya <ricunsos@gmail.com>
2021-11-16 11:15:19 +0300

    book dto

--- backend/src/main/kotlin/com/example/backend/dto/Book.kt
+++ backend/src/main/kotlin/com/example/backend/dto/Book.kt
@@ -2,21 +2,39 @@ package com.example.backend.dto
 
 import org.springframework.data.annotation.Id
 
-
+/**
+ * DTO - Data Transfer Object.
+ * Представление сущности - книга.
+ *
+ * */
 data class Book(
         @Id var id: Long? = null,
-        var text: String? = null) {
+        var name: String? = null,
+        var author: String? = null,
+        var numberOfPages : Int? = null,
+        var isElectronic : Boolean? = null
+) {
 
-//    data class Builder(
-//            var id: Long? = null,
-//            var text: String? = null
-//    ) {
-//
-//        fun text(id: Long?) = apply { this.id = id }
-//        fun text(text: String?) = apply { this.text = text }
-//
-//        fun build() = Book(
-//                id,
-//                text)
-//    }
+    data class Builder(
+            var id: Long? = null,
+            var name: String? = null,
+            var author: String? = null,
+            var numberOfPages: Int? = null,
+            var isElectronic: Boolean? = null
+    ) {
+
+        fun text(id: Long?) = apply { this.id = id }
+        fun name(name: String?) = apply { this.name = name }
+        fun author(author: String?) = apply { this.author = author }
+        fun numberOfPages(numberOfPages: Int?) = apply { this.numberOfPages = numberOfPages }
+        fun isElectronic(isElectronic: Boolean?) = apply { this.isElectronic = isElectronic }
+
+        fun build() = Book(
+                id,
+                name,
+                author,
+                numberOfPages,
+                isElectronic
+        )
+    }
 }

1830921d8bc7395423a7b9f86b4be300ed5c99f7
Rustam Kuramshin <kuramshin.py@yandex.ru>
2021-11-18 00:16:34 +0300

    add table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import './App.css';
-import { Button } from "@blueprintjs/core";
+import { Cell, Column, Table2 } from "@blueprintjs/table";
 
 import 'normalize.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/table/lib/css/table.css';
+
+const USD_TO_EURO_CONVERSION = 0.85;
+const dollarCellRenderer = (rowIndex: number) => <Cell>{`$${(rowIndex * 10).toFixed(2)}`}</Cell>;
+const euroCellRenderer = (rowIndex: number) => (<Cell>{`€${(rowIndex * 10 * USD_TO_EURO_CONVERSION).toFixed(2)}`}</Cell>);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button intent="success" text="button content" />
+        <Table2 numRows={20}>
+          <Column name="Dollars" cellRenderer={dollarCellRenderer} />
+          <Column name="Euros" cellRenderer={euroCellRenderer} />
+        </Table2>
       </header>
     </div>
   );

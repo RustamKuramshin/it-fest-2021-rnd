9617b873a46e6fcc4aff728c0c8c925a78265bad
rustam <kuramshin.py@yandex.ru>
2021-11-21 00:12:16 +0300

    fix script

--- start.sh
+++ start.sh
@@ -5,14 +5,14 @@ if [ -x "$(command -v docker)" ]; then
     docker -v
 else
     curl -fsSL https://get.docker.com -o get-docker.sh && \
-    sudo DRY_RUN=1 sh ./get-docker.sh
+    sudo sh ./get-docker.sh
 fi
 
 if [ -x "$(command -v docker-compose)" ]; then
     echo "Docker Compose already installed"
     docker-compose -v
 else
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
     sudo chmod +x /usr/local/bin/docker-compose
 fi
 
